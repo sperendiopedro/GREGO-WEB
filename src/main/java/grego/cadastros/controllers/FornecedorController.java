90bcc75d32b5719181595eb3e6363f869c1fe9d0
pedro <pedro.sperendio@outlook.com>
2024-08-22 15:15:48 -0300

    alterações fornecedor, ainda fica constraint para arrumar

--- src/main/java/grego/cadastros/controllers/FornecedorController.java
+++ src/main/java/grego/cadastros/controllers/FornecedorController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +24,7 @@ import grego.cadastros.repositories.UFDRepository;
 
 @RestController
 @RequestMapping("fornec")
+@CrossOrigin(origins = "*")
 public class FornecedorController {
 
 	@Autowired
