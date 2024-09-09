bbd50e975e68bd72354dd2558d42f183cf1b8916
pedro <pedro.sperendio@outlook.com>
2024-09-09 15:51:29 -0300

    alterações 09-09

--- src/main/java/grego/users/controller/UserController.java
+++ src/main/java/grego/users/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,34 +26,33 @@ import grego.users.repositories.UserRepository;
 @RequestMapping("/user")
 @CrossOrigin(origins = "*")
 public class UserController {
-	
-	@Autowired
-	private PasswordEncoder passwdEncoder; 
-	
-	
-	@Autowired
-	private UserRepository userRepo; 
-	
-	@GetMapping("/list")
-	public List<User> listAll(){
-		return userRepo.findAll(); 
-	}
-	
-	@PostMapping("/register")
-	public ResponseEntity<?> register(@RequestBody User user) {
-	    Optional<User> optUser = userRepo.findByEmail(user.getEmail());
-	    if(optUser.isPresent()) {
-	        return new ResponseEntity<>(Map.of("message", "Usuário já existe no sistema!"), HttpStatus.CONFLICT);
-	    }
-	    try {
-	        user.setPassword(passwdEncoder.encode(user.getPassword())); 
-	        user.setUserRole(user.getUserRole().toUpperCase());
-	        userRepo.saveAndFlush(user);
-	        return ResponseEntity.ok(Map.of("message", "Usuario salvo com sucesso!"));
-	    } catch (Exception e) {
-	        return new ResponseEntity<>(Map.of("message", "Erro ao salvar o usuário!", "error", e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
-	    }
-	}
-	
 
+	@Autowired
+	private PasswordEncoder passwdEncoder;
+
+	@Autowired
+	private UserRepository userRepo;
+
+	@GetMapping("/list")
+	public List<User> listAll() {
+		return userRepo.findAll();
+	}
+
+	@PostMapping("/register")
+	public ResponseEntity<String> register(@RequestBody User user) {
+		Optional<User> optUser = userRepo.findByEmail(user.getEmail());
+		if (optUser.isPresent()) {
+			return new ResponseEntity<>("Usuário já existe no sistema!", HttpStatus.CONFLICT);
+		}
+		try {
+			user.setPassword(passwdEncoder.encode(user.getPassword()));
+			if(user.getUserRole() == null) {
+				user.setUserRole("USER"); 
+			}
+			userRepo.saveAndFlush(user);
+			return new ResponseEntity<>("Usuario salvo com sucesso!", HttpStatus.OK);	
+		} catch (Exception e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 }
